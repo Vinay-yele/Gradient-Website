515f0cbb2fda2fb9b80845913eabe9c7f3a37580
pranavisda1 <pranavisda1@gmail.com>
2025-04-18 23:47:22 +0530

    18/4

--- app/gw25/page.tsx
+++ app/gw25/page.tsx
@@ -1,14 +1,11 @@
-'use client'
+'use client';
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 const RedirectOnLoad: React.FC = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
-    // Redirect to a different route on component load
-    navigate("https://gradient-week.vercel.app");
-  }, [navigate]);
+    // Redirect to an external URL on component load
+    window.location.href = "https://gradient-week.vercel.app";
+  }, []);
 
   return null; // No UI needed since this component just redirects
 };
