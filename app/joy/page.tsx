07cf2bad539c9cc52ef9d783bf7f74c3904559d4
pranavisda1 <pranavisda1@gmail.com>
2025-05-05 20:49:34 +0530

    05-05-2025

--- app/joy/page.tsx
+++ app/joy/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const RedirectOnLoad: React.FC = () => {
   useEffect(() => {
     // Redirect to an external URL on component load
-    window.location.href = "https://events.bmsutsav.in/events/GRDJOY;
+    window.location.href = "https://events.bmsutsav.in/events/GRDJOY";
   }, []);
 
   return null; // No UI needed since this component just redirects
